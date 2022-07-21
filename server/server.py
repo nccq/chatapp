6d55bc28324c7dc03abc09f0b1644e042f9d13f0
nccq <lukstas17@gmail.com>
2022-07-21 21:56:15 +0300

    server done

--- server/server.py
+++ server/server.py
@@ -1,28 +1,49 @@
 from socket import socket, AF_INET, SOCK_STREAM
 from threading import Thread
 import time
+from unicodedata import name
 
-from server.account import account
+from account import account
 
 
 HOST = 'localhost'
 PORT = 5500
 BUFSIZE = 512
 ADDR = (HOST , PORT)
+accounts = []
+SERVER = socket(AF_INET , SOCK_STREAM)
+SERVER.bind(ADDR)
 
 
-def broadcast():
+def broadcast(msg, name):
+    for account in accounts:
+        client = account.client
+        client.send(bytes(name + ":", "utf8") + msg)
 
 
 
 def handle_client(account):
-    name = account.name
-    ADDR = account.ADDR
+    client = account.client
+    name = client.recv(BUFSIZE).decode("urf8")
+    msg = f"{name} has joind the chat"
+    broadcast(msg)
+
+
     while True:
-        msg = account.recv(BUFSIZE)
-        if msg == bytes("{Quit}", "utf8"):
-            account.close()
-        else:
+        try:
+            msg = client.recv(BUFSIZE)
+            print(f"{name}:", msg.decode("utf8"))
+            if msg == bytes("{Quit}", "utf8"):
+                broadcast(f"{name} has left the chat", "")
+                client.send(bytes("{Quit}", "utf8"))
+                client.close()
+                accounts.remove(account)
+                break
+            else:
+                broadcast(msg, name)
+        except Exception as e:
+             print("[EXCEPTION]", e)
+             break
 
 
 
@@ -33,17 +54,14 @@ def wait_for_connection():
         try:
             client, ADDR = SERVER.accept()
             account = account(ADDR, name, client)
+            accounts.append(account)
             print(f"[CONNECTION] {ADDR} connected to the server at {time.time()}")
             Thread(target=handle_client, args=(account)).start()
         except Exception as e:
-            print("[FAILURE]", e)
+            print("[EXCEPTION]", e)
             run = False
 
     print("SERVER CRASHED")
-
-
-SERVER = socket(AF_INET , SOCK_STREAM)
-SERVER.bind(ADDR)
 
 
 if __name__ == '__main__':
