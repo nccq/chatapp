0a3e2e7b455868cee642c300801219b7650d3e81
nccq <lukstas17@gmail.com>
2022-07-26 21:46:03 +0300

    client.py test

---
+++ client/client.py
@@ -0,0 +1,46 @@
+from socket import socket, AF_INET, SOCK_STREAM
+from threading import Thread, Lock
+import time
+
+
+class client:
+
+    HOST = 'localhost'
+    PORT = 5500
+    BUFSIZE = 512
+    ADDR = (HOST , PORT)
+
+    def __init__(self, name):
+        self.client_socket = socket(AF_INET, SOCK_STREAM)
+        self.client_socket.connect(self.ADDR)
+        self.messages = []
+        recive_thread = Thread(target=self.recive_messages)
+        recive_thread.start()
+        self.send_message(name)
+        self.lock = Lock()
+
+
+    def recive_messages(self):
+        while True:
+            try:
+                msg = self.client_socket.recv(self.BUFSIZE).decode()
+                self.lock.acquire()
+                self.messages.append(msg)
+                self.lock.release()
+                print(msg)
+            except Exception as e:
+                print("EXCEPTION", e)
+                break
+
+
+    def send_message(self, msg):
+        self.client_socket.send(bytes(msg, "utf8"))
+        if msg == "{quit}":
+            self.client_socket.close()
+
+
+    def get_messages(self):
+        self.lock.acquire()
+        self.lock.release()
+        return self.messages
+
