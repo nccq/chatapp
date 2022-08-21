0dd5f8498141519d84cab5f54ac3839a1c9c241e
nccq <lukstas17@gmail.com>
2022-08-21 19:45:59 +0300

    templates finished, redoing server

--- web/main.py
+++ web/main.py
@@ -1,37 +1,7 @@
-from flask import Flask, render_template, url_for, redirect, request, session
+from flask import session
+from flask_socketio import SocketIO
 
 
-NAME_KEY = "name"
+app = create_app()
+socketio = SocketIO(app)
 
-
-app = Flask(__name__)
-app.secret_key = "cloudy"
-
-'''
-@app.route("/login")
-def login():
-    return render_template("login.html")'''
-
-
-@app.route("/logout")
-def logout():
-    session.pop(NAME_KEY, None)
-    return redirect(url_for("login"))
-
-
-@app.route("/")
-@app.route("/home")
-def home():
-    #if NAME_KEY not in session:
-     #   return redirect(url_for("login"))
-
-    #name = session[NAME_KEY]
-    return render_template("index.html") 
-
-
-def run():
-    print("clicked")
-
-
-if __name__ == "__main__":
-    app.run(debug = True)
