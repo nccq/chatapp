59820db1d13a440a44390a0005fc5939b0bc78ff
nccq <lukstas17@gmail.com>
2022-08-22 08:03:43 +0300

    config and .env created

---
+++ web/config.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+from pathlib import Path  # python3 only
+import os
+
+# set path to env file
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+class Config:
+    TESTING = os.getenv('TESTING')
+    FLASK_DEBUG = os.getenv('FLASK_DEBUG')
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SERVER = os.getenv('SERVER')
